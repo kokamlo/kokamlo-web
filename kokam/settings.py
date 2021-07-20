7e02aac2e0cbdce365f41dee58697cabf4d1969c
kokamlo <behzad0hosseinzadeh@gmail.com>
2021-07-20 12:58:34 +0000

    remove ad

--- kokam/settings.py
+++ kokam/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9uk7u4l#-v*sazq$$a_zm)f-9cpwp1^6!8oa8-g052^b^h)mp%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kokamlo.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
